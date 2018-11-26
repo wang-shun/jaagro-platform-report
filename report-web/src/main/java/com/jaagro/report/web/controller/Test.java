93889aae4c80fdad70db3c802436f494cf7d8fa3
yogasan <yoga330356233@126.com>
2018-11-26 17:28:45 +0800

    初始化report实体和mapper

--- report-web/src/main/java/com/jaagro/report/web/controller/Test.java
+++ report-web/src/main/java/com/jaagro/report/web/controller/Test.java
@@ -2,9 +2,8 @@ package com.jaagro.report.web.controller;
 
 import com.jaagro.report.api.annotation.DataSource;
 import com.jaagro.report.biz.entity.DriverOrderDaily;
-import com.jaagro.report.biz.entity.UserLogin;
-import com.jaagro.report.biz.mapper.DriverOrderDailyMapper;
-import com.jaagro.report.biz.mapper.UserLoginMapper;
+import com.jaagro.report.biz.mapper.report.DriverOrderDailyMapper;
+import com.jaagro.report.biz.mapper.report.UserLoginMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
